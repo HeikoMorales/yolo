34b278bd57d43caa40def9cac93d3538d0ccae72
HeikoMorales <heikomorales2@gmail.com>
2024-05-06 20:43:52 +0200

    tiene buena pinta

--- enviarImagenesHadoop.py
+++ enviarImagenesHadoop.py
@@ -1,13 +1,13 @@
 from hdfs import InsecureClient
 
 # Conexión al sistema de archivos Hadoop (HDFS)
-client = InsecureClient('http://localhost:9870', user='root')
+client = InsecureClient('http://namenode:9870', user='hadoop')
 
 # Ruta local de la carpeta que deseas copiar a Hadoop
 carpeta_local = "./runs/pose/images"
 
 # Ruta en HDFS donde deseas copiar la carpeta
-carpeta_hdfs = "/test"
+carpeta_hdfs = "/user"
 
 # Copia la carpeta y su contenido a HDFS
 client.upload(hdfs_path=carpeta_hdfs, local_path=carpeta_local)
